5dcf31219f00e7aa2b1b5b79c4c69bcdf61fa02b
isanae <14251494+isanae@users.noreply.github.com>
2020-07-31 18:37:25 -0400

    appmanifest file is utf8

--- steam_utils.py
+++ steam_utils.py
@@ -27,7 +27,7 @@ class LibraryFolder:
         self.games = []
         for filename in os.listdir(os.path.join(path, "steamapps")):
             if filename.startswith("appmanifest"):
-                with open(os.path.join(path, "steamapps", filename), "r") as fp:
+                with open(os.path.join(path, "steamapps", filename), "r", encoding="utf-8") as fp:
                     i, n = None, None
                     for line in fp:
                         line = line.strip()
