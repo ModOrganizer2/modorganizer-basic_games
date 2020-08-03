4d3709fc1bdd102b877a12e9c72a193df651035d
EzioTheDeadPoet <52624146+EzioTheDeadPoet@users.noreply.github.com>
2020-08-04 00:22:09 +0200

    set the whole directory as the "DATA" Directory
    To make it more usable at modding the game.

--- games/game_witcher3.py
+++ games/game_witcher3.py
@@ -20,7 +20,7 @@ class Witcher3Game(BasicGame):
     GameNexusId = 952
     GameSteamId = 292030
     GameBinary = "bin/x64/witcher3.exe"
-    GameDataPath = "Mods"
+    GameDataPath = ""
     GameSaveExtension = "sav"
     GameDocumentsDirectory = "%DOCUMENTS%/The Witcher 3"
     GameSavesDirectory = "%GAME_DOCUMENTS%/gamesaves"
