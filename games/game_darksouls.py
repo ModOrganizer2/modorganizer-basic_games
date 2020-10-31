0f567e09f4db4d060d490cdde879786ee8c015ac
Mikaël Capelle <capelle.mikael@gmail.com>
2020-10-31 14:25:54 +0100

    Add Dark Souls plugin.

---
+++ games/game_darksouls.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+
+from PyQt5.QtCore import QDir, QFileInfo, QStandardPaths
+
+import mobase
+
+from ..basic_game import BasicGame
+
+
+class DarkSoulsGame(BasicGame):
+    Name = "DarkSouls"
+    Author = "Holt59"
+    Version = "0.1.0"
+
+    GameName = "Dark Souls"
+    GameShortName = "darksouls"
+    GameNexusName = "darksouls"
+    GameNexusId = 162
+    GameSteamId = 211420
+    GameBinary = "DATA/DARKSOULS.exe"
+    GameDataPath = "DATA"
+    GameDocumentsDirectory = "%DOCUMENTS%/NBGI/DarkSouls"
+    GameSavesDirectory = "%DOCUMENTS%/NBGI/DarkSouls"
+    GameSaveExtension = "sl2"
