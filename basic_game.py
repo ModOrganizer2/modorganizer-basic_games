33821dba33fb46cebcb4e813acb1c402ae8e4824
Mikaël Capelle <capelle.mikael@gmail.com>
2020-11-10 18:11:35 +0100

    Consider plugin inactive if there is no managed game.

--- basic_game.py
+++ basic_game.py
@@ -347,6 +347,9 @@ class BasicGame(mobase.IPluginGame):
         return self._mappings.version.get()
 
     def isActive(self) -> bool:
+        if not self._organizer.managedGame():
+            return False
+
         # Note: self is self._organizer.managedGame() does not work:
         return self.name() == self._organizer.managedGame().name()
 
