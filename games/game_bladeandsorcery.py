7a205328f03faa34d19886a3ca235aceed3e7702
Mikaël Capelle <capelle.mikael@gmail.com>
2021-03-02 19:25:13 +0100

    Add Blade & Sorcery support (R3z Shark).

---
+++ games/game_bladeandsorcery.py
@@ -0,0 +1,14 @@
+from ..basic_game import BasicGame
+
+
+class BaSGame(BasicGame):
+
+    Name = "Blade & Sorcery Plugin"
+    Author = "R3z Shark"
+    Version = "0.1.0"
+
+    GameName = "Blade & Sorcery"
+    GameShortName = "bladeandsorcery"
+    GameBinary = "BladeAndSorcery.exe"
+    GameDataPath = r"BladeAndSorcery_Data\StreamingAssets\Mods"
+    GameSteamId = 629730
