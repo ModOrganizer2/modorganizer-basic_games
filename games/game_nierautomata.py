af3f777b6ad6f2111a0ac249b7d1bde64e3f07e5
Luca <EzioTheDeadPoet@users.noreply.github.com>
2021-05-10 22:50:17 +0200

    added very basic Nier:Automata support

---
+++ games/game_nierautomata.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+from ..basic_game import BasicGame
+
+
+class NierAutomataGame(BasicGame):
+
+    Name = "NieR:Automata Support Plugin"
+    Author = "Luca/EzioTheDeadPoet"
+    Version = "1.0.0"
+
+    GameName = "NieR:Automata"
+    GameShortName = "nierautomata"
+    GaneNexusHame = "nierautomata"
+    GameSteamId = 524220
+    GameBinary = "NieRAutomata.exe"
+    GameDataPath = ""
