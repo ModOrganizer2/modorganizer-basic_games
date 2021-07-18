e3f28b8a27bfa3ad474c06b6a2ae5567b7882d97
Mikaël Capelle <capelle.mikael@gmail.com>
2021-07-18 10:33:36 +0200

    Add VDF dependency.

--- __init__.py
+++ __init__.py
@@ -3,11 +3,15 @@
 import glob
 import importlib
 import os
+import site
 import sys
 import typing
 
 from .basic_game import BasicGame
 from .basic_game_ini import BasicIniGame
+
+site.addsitedir(os.path.join(os.path.dirname(__file__), "lib"))
+
 
 BasicGame.setup()
 
