e823528d83c49454dffc095a20bbbdf584111221
Zash <zash@fantasymail.de>
2022-03-30 21:07:54 +0200

    + subnautica: BasicModDataChecker

--- games/game_subnautica.py
+++ games/game_subnautica.py
@@ -1,19 +1,50 @@
+from __future__ import annotations
+
+import mobase
+
+from ..basic_features.basic_mod_data_checker import BasicModDataChecker, FilePatterns
 from ..basic_game import BasicGame
+
+subnautica_file_patterns = FilePatterns(
+    set_as_root={"BepInExPack_Subnautica"},
+    valid={"winhttp.dll", "doorstop_config.ini", "BepInEx", "QMods"},
+    delete={
+        "*.txt",
+        "*.md",
+        "icon.png",
+        "license",
+        "manifest.json",
+    },
+    move={"plugins": "BepInEx/", "patchers": "BepInEx/", "*": "QMods/"},
+)
 
 
 class SubnauticaGame(BasicGame):
 
     Name = "Subnautica Support Plugin"
-    Author = "dekart811"
-    Version = "1.0"
+    Author = "dekart811, Zash"
+    Version = "2.0"
 
     GameName = "Subnautica"
     GameShortName = "subnautica"
     GameNexusName = "subnautica"
     GameSteamId = 264710
     GameBinary = "Subnautica.exe"
-    GameDataPath = "QMods"
-    GameDocumentsDirectory = "%GAME_PATH%"
+    GameDataPath = ""
+    GameDocumentsDirectory = r"%GAME_PATH%"
+    GameSavesDirectory = r"%GAME_PATH%\SNAppData\SavedGames"
 
-    def iniFiles(self):
-        return ["doorstop_config.ini"]
+    _forced_libraries = ["winhttp.dll"]
+
+    def init(self, organizer: mobase.IOrganizer) -> bool:
+        super().init(organizer)
+        self._featureMap[mobase.ModDataChecker] = BasicModDataChecker(
+            subnautica_file_patterns
+        )
+        return True
+
+    def executableForcedLoads(self) -> list[mobase.ExecutableForcedLoadSetting]:
+        return [
+            mobase.ExecutableForcedLoadSetting(self.binaryName(), lib).withEnabled(True)
+            for lib in self._forced_libraries
+        ]
