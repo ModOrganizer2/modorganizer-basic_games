94ec836fe359789dda8ecd496615e59393512730
Zash <zash@fantasymail.de>
2022-03-30 21:08:55 +0200

    + save preview for subnautica

--- games/game_subnautica.py
+++ games/game_subnautica.py
@@ -1,8 +1,17 @@
 from __future__ import annotations
 
+import os
+from pathlib import Path
+
+from PyQt5.QtCore import QDir
+
 import mobase
 
 from ..basic_features.basic_mod_data_checker import BasicModDataChecker, FilePatterns
+from ..basic_features.basic_save_game_info import (
+    BasicGameSaveGame,
+    BasicGameSaveGameInfo,
+)
 from ..basic_game import BasicGame
 
 subnautica_file_patterns = FilePatterns(
@@ -41,7 +50,24 @@ class SubnauticaGame(BasicGame):
         self._featureMap[mobase.ModDataChecker] = BasicModDataChecker(
             subnautica_file_patterns
         )
+        self._featureMap[mobase.SaveGameInfo] = BasicGameSaveGameInfo(
+            lambda s: os.path.join(s, "screenshot.jpg")
+        )
         return True
+
+    def listSaves(self, folder: QDir) -> list[mobase.ISaveGame]:
+        savegames = super().listSaves(folder)
+        for save_path in [
+            folder.absolutePath(),
+            os.path.expandvars(
+                r"%USERPROFILE%\Appdata\LocalLow\Unknown Worlds"
+                r"\Subnautica\Subnautica\SavedGames"
+            ),
+        ]:
+            savegames.extend(
+                BasicGameSaveGame(folder) for folder in Path(save_path).glob("slot*")
+            )
+        return savegames
 
     def executableForcedLoads(self) -> list[mobase.ExecutableForcedLoadSetting]:
         return [
