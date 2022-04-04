6f8033e1b2ba09b221e9c06318ee4719a51cb1d0
Zash <zash@fantasymail.de>
2022-04-04 17:59:18 +0200

    Subnautica: + BasicModDataChecker + root data path + savegame preview

--- games/game_subnautica.py
+++ games/game_subnautica.py
@@ -1,11 +1,40 @@
+from __future__ import annotations
+
+import os
+from pathlib import Path
+
+from PyQt5.QtCore import QDir
+
+import mobase
+
+from ..basic_features import BasicModDataChecker
+from ..basic_features.basic_save_game_info import (
+    BasicGameSaveGame,
+    BasicGameSaveGameInfo,
+)
 from ..basic_game import BasicGame
+
+
+class SubnauticaModDataChecker(BasicModDataChecker):
+    default_file_patterns = {
+        "unfold": ["BepInExPack_Subnautica"],
+        "valid": ["winhttp.dll", "doorstop_config.ini", "BepInEx", "QMods"],
+        "delete": [
+            "*.txt",
+            "*.md",
+            "icon.png",
+            "license",
+            "manifest.json",
+        ],
+        "move": {"plugins": "BepInEx/", "patchers": "BepInEx/", "*": "QMods/"},
+    }
 
 
 class SubnauticaGame(BasicGame):
 
     Name = "Subnautica Support Plugin"
-    Author = "dekart811"
-    Version = "1.0"
+    Author = "dekart811, Zash"
+    Version = "2.0"
 
     GameName = "Subnautica"
     GameShortName = "subnautica"
@@ -13,12 +42,41 @@ class SubnauticaGame(BasicGame):
     GameSteamId = 264710
     GameEpicId = "Jaguar"
     GameBinary = "Subnautica.exe"
-    GameDataPath = "QMods"
+    GameDataPath = ""
     GameDocumentsDirectory = "%GAME_PATH%"
     GameSupportURL = (
         r"https://github.com/ModOrganizer2/modorganizer-basic_games/wiki/"
         "Game:-Subnautica"
     )
+    GameSavesDirectory = r"%GAME_PATH%\SNAppData\SavedGames"
 
-    def iniFiles(self):
-        return ["doorstop_config.ini"]
+    _game_extra_save_paths = [
+        r"%USERPROFILE%\Appdata\LocalLow\Unknown Worlds"
+        r"\Subnautica\Subnautica\SavedGames"
+    ]
+
+    _forced_libraries = ["winhttp.dll"]
+
+    def init(self, organizer: mobase.IOrganizer) -> bool:
+        super().init(organizer)
+        self._featureMap[mobase.ModDataChecker] = SubnauticaModDataChecker()
+        self._featureMap[mobase.SaveGameInfo] = BasicGameSaveGameInfo(
+            lambda s: os.path.join(s, "screenshot.jpg")
+        )
+        return True
+
+    def listSaves(self, folder: QDir) -> list[mobase.ISaveGame]:
+        return [
+            BasicGameSaveGame(folder)
+            for save_path in (
+                folder.absolutePath(),
+                *(os.path.expandvars(p) for p in self._game_extra_save_paths),
+            )
+            for folder in Path(save_path).glob("slot*")
+        ]
+
+    def executableForcedLoads(self) -> list[mobase.ExecutableForcedLoadSetting]:
+        return [
+            mobase.ExecutableForcedLoadSetting(self.binaryName(), lib).withEnabled(True)
+            for lib in self._forced_libraries
+        ]
