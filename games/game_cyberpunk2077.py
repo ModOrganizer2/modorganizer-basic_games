6722e975f95affad63e121152bf63e8137da6f14
Zash <zash@fantasymail.de>
2022-11-19 16:58:24 +0100

    fix: Cyberpunk ini files (path)

--- games/game_cyberpunk2077.py
+++ games/game_cyberpunk2077.py
@@ -3,15 +3,16 @@ from ..basic_game import BasicGame
 
 class Cyberpunk2077Game(BasicGame):
     Name = "Cyberpunk 2077 Support Plugin"
-    Author = "6788"
-    Version = "1.0.0"
+    Author = "6788, Zash"
+    Version = "1.1.0"
 
     GameName = "Cyberpunk 2077"
     GameShortName = "cyberpunk2077"
     GameBinary = "bin/x64/Cyberpunk2077.exe"
     GameLauncher = "REDprelauncher.exe"
     GameDataPath = "%GAME_PATH%"
-    GameDocumentsDirectory = "%USERPROFILE%/Saved Games/CD Projekt Red/Cyberpunk 2077"
+    GameDocumentsDirectory = "%USERPROFILE%/AppData/Local/CD Projekt Red/Cyberpunk 2077"
+    GameSavesDirectory = "%USERPROFILE%/Saved Games/CD Projekt Red/Cyberpunk 2077"
     GameSaveExtension = "dat"
     GameSteamId = 1091500
     GameGogId = 1423049311
@@ -19,3 +20,6 @@ class Cyberpunk2077Game(BasicGame):
         r"https://github.com/ModOrganizer2/modorganizer-basic_games/wiki/"
         "Game:-Cyberpunk-2077"
     )
+
+    def iniFiles(self):
+        return ["UserSettings.json"]
