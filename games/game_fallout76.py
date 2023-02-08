8a7b2d01fa90c5e5124a48d09dc2c9279ae22c66
TDarkShadow <5389859+TDarkShadow@users.noreply.github.com>
2023-02-08 16:51:18 +0100

    Add F76 Data folder location & config files

--- games/game_fallout76.py
+++ games/game_fallout76.py
@@ -5,13 +5,16 @@ class Fallout76Game(BasicGame):
 
     Name = "Fallout 76 Support Plugin"
     Author = "TDarkShadow"
-    Version = "1.0.0"
+    Version = "1.0.1"
 
     GameName = "Fallout 76"
     GameShortName = "f76"
     GameBinary = "Fallout76.exe"
-    GameDataPath = "%GAME_PATH%"
+    GameDataPath = "%GAME_PATH%/Data"
     GameDocumentsDirectory = "%DOCUMENTS%/My Games/Fallout 76"
     GameSteamId = 1151340
     GameNexusId = 2590
     GameNexusName = "fallout76"
+
+    def iniFiles(self):
+        return ["Fallout76.ini, Fallout76Prefs.ini, Fallout76Custom.ini"]
