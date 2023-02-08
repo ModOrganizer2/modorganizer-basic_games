12cf3c66f7081f4f6138b3c3d967240d354eb002
TDarkShadow <5389859+TDarkShadow@users.noreply.github.com>
2023-02-08 11:35:40 +0100

    Add Fallout 76 support

---
+++ games/game_fallout76.py
@@ -0,0 +1,17 @@
+from ..basic_game import BasicGame
+
+
+class Fallout76Game(BasicGame):
+
+    Name = "Fallout 76 Support Plugin"
+    Author = "TDarkShadow"
+    Version = "1.0.0"
+
+    GameName = "Fallout 76"
+    GameShortName = "f76"
+    GameBinary = "Fallout76.exe"
+    GameDataPath = "%GAME_PATH%"
+    GameDocumentsDirectory = "%DOCUMENTS%/My Games/Fallout 76"
+    GameSteamId = 1151340
+    GameNexusId = 2590
+    GameNexusName = "fallout76"
