7d4002eb9c57b85947537b2799b357ca41f5ed3d
kane <douqilong@gmail.com>
2023-09-23 13:03:15 +0000

    Sekiro: Shadows Die Twice Support Plugin

---
+++ games/game_sekiroshadowsdietwice.py
@@ -0,0 +1,14 @@
+from ..basic_game import BasicGame
+
+
+class SekiroShadowsDieTwiceGame(BasicGame):
+    Name = "Sekiro: Shadows Die Twice Support Plugin"
+    Author = "Kane Dou"
+    Version = "1.0.0"
+
+    GameName = "Sekiro: Shadows Die Twice"
+    GameShortName = "sekiro"
+    GameBinary = "sekiro.exe"
+    GameDataPath = "mods"
+    GameSaveExtension = "sl2"
+    GameSteamId = 814380
