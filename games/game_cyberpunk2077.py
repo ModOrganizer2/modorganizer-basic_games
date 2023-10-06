f777ba56c77c57659e793c643910d1eab6115739
Zash <zash@fantasymail.de>
2023-10-06 03:55:50 +0200

    Add ModDataChecker mod support: archive, redscript, RED4ext and CET v1.27.0 (Patch 2.01) moves RED4ext and CET framework to root folder

--- games/game_cyberpunk2077.py
+++ games/game_cyberpunk2077.py
@@ -1,16 +1,150 @@
 import json
+import re
 from pathlib import Path
 
 import mobase
 from PyQt6.QtCore import QDir
 
-from ..basic_features import BasicLocalSavegames
+from ..basic_features import BasicLocalSavegames, BasicModDataChecker, GlobPatterns
 from ..basic_features.basic_save_game_info import (
     BasicGameSaveGame,
     BasicGameSaveGameInfo,
     format_date,
 )
+from ..basic_features.utils import is_directory
 from ..basic_game import BasicGame
+
+
+class CyberpunkModDataChecker(BasicModDataChecker):
+    def __init__(self):
+        super().__init__(
+            GlobPatterns(
+                valid=[
+                    "archive",
+                    # redscript
+                    "engine",
+                    "r6",
+                    "mods",  # RedMod
+                    "red4ext",  # red4ext/RED4ext.dll is moved to root in .fix()
+                    "bin",  # CET etc. gets handled below
+                    "root",  # RootBuilder: hardlink / copy to game root
+                ],
+                move={
+                    # archive and ArchiveXL
+                    "*.archive": "archive/pc/mod/",
+                    "*.xl": "archive/pc/mod/",
+                },
+            )
+        )
+
+    _extra_files_to_move = {
+        # Red4ext: only .dll files
+        "red4ext/RED4ext.dll": "root/red4ext/",
+        "bin/x64/winmm.dll": "root/bin/x64/",
+        # CET: all files, folder gets handled in .fix()
+        "bin/x64/version.dll": "root/bin/x64/",
+        "bin/x64/global.ini": "root/bin/x64/",
+        "bin/x64/plugins/cyber_engine_tweaks.asi": "root/bin/x64/plugins/",
+    }
+    """Some frameworks need to be copied or hard linked to root. Use / as sep!"""
+    _ignore_pattern = re.compile(r"licenses?$", re.I)
+
+    _cet_path = "bin/x64/plugins/cyber_engine_tweaks/"
+
+    def dataLooksValid(
+        self, filetree: mobase.IFileTree
+    ) -> mobase.ModDataChecker.CheckReturn:
+        # fix: single root folders get traversed by Simple Installer
+        parent = filetree.parent()
+        if parent is not None and self.dataLooksValid(parent) is self.FIXABLE:
+            return self.FIXABLE
+        if (status := super().dataLooksValid(filetree)) is not self.INVALID:
+            match self._check_bin_folder(filetree):
+                case self.INVALID:
+                    return self.INVALID
+                case self.FIXABLE:
+                    status = self.FIXABLE
+                case _:
+                    pass  # valid = keep status
+            # Check extra fixes
+            if any(filetree.exists(p) for p in self._extra_files_to_move):
+                status = self.FIXABLE
+        return status
+
+    def _check_bin_folder(
+        self, filetree: mobase.IFileTree
+    ) -> mobase.ModDataChecker.CheckReturn:
+        """Only Red4ext and CET are supported in bin folder."""
+        bin_folder = filetree.find("bin/x64")
+        if not bin_folder:
+            return self.VALID
+        elif not is_directory(bin_folder):
+            return self.INVALID
+        status = self.VALID
+        cet_path = self._cet_path.rstrip("/\\")
+        for entry in bin_folder:
+            entry_name = entry.name()
+            if self._ignore_pattern.match(entry_name):
+                continue
+            elif f"bin/x64/{entry_name}" in self._extra_files_to_move:
+                status = self.FIXABLE
+            elif entry_name == "plugins" and is_directory(entry):
+                for plugin in entry:
+                    plugin_path = f"bin/x64/plugins/{plugin.name()}"
+                    if plugin_path == cet_path:
+                        if not is_directory(plugin):
+                            return self.INVALID
+                        if not (len(plugin) == 1 and plugin.exists("mods")):
+                            status = self.FIXABLE  # CET framework: fix
+                    elif plugin_path in self._extra_files_to_move:
+                        status = self.FIXABLE
+                    else:
+                        return self.INVALID  # unknown plugin
+            else:
+                return self.INVALID  # unknown entry
+        return status
+
+    def fix(self, filetree: mobase.IFileTree) -> mobase.IFileTree:
+        if filetree := super().fix(filetree):
+            for source, target in self._extra_files_to_move.items():
+                if file := filetree.find(source):
+                    parent = file.parent()
+                    filetree.move(file, target)
+                    clear_empty_folder(parent)
+            self._fix_cet_framework(filetree)
+        return filetree
+
+    def _fix_cet_framework(self, filetree: mobase.IFileTree):
+        """Move CET framework to `root/`, except for `mods`.
+        Only CET >= v1.27.0 (Patch 2.01) works with USVFS.
+
+        See: https://github.com/maximegmd/CyberEngineTweaks/pull/877
+        """
+        if cet_folder := filetree.find(
+            self._cet_path, mobase.FileTreeEntry.FileTypes.DIRECTORY
+        ):
+            assert is_directory(cet_folder)
+            root_cet_path = f"root/{self._cet_path}"
+            if not cet_folder.exists("mods"):
+                parent = cet_folder.parent()
+                filetree.move(cet_folder, root_cet_path.rstrip("/\\"))
+            else:
+                parent = cet_folder
+                for entry in list(cet_folder):
+                    if entry.name() != "mods":
+                        filetree.move(entry, root_cet_path)
+            clear_empty_folder(parent)
+
+
+def clear_empty_folder(filetree: mobase.IFileTree | None):
+    if filetree is None:
+        return
+    while not filetree:
+        parent = filetree.parent()
+        filetree.detach()
+        if parent is None:
+            break
+        filetree = parent
 
 
 def time_from_seconds(s: int | float) -> str:
@@ -21,7 +155,6 @@ def time_from_seconds(s: int | float) -> str:
 
 def parse_cyberpunk_save_metadata(save_path: Path, save: mobase.ISaveGame):
     metadata_file = save_path / "metadata.9.json"
-
     try:
         with open(metadata_file) as file:
             meta_data = json.load(file)["Data"]["metadata"]
@@ -88,6 +221,7 @@ class Cyberpunk2077Game(BasicGame):
             lambda p: Path(p or "", "screenshot.png"),
             parse_cyberpunk_save_metadata,
         )
+        self._featureMap[mobase.ModDataChecker] = CyberpunkModDataChecker()
         return True
 
     def listSaves(self, folder: QDir) -> list[mobase.ISaveGame]:
