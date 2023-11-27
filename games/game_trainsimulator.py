2ad6e3dd876257374ba4b531594978aaa936ead4
Ryan Young <ryan@youngryan.com>
2023-11-27 20:09:39 +0000

    Add documentation and dx12 executable for Train Simulator Classic

--- games/game_trainsimulator.py
+++ games/game_trainsimulator.py
@@ -5,28 +5,31 @@ from ..basic_game import BasicGame
 
 
 class RailworksGame(BasicGame):
-    Name = "Train Simulator 20xx Support Plugin"
+    Name = "Train Simulator Classic Support Plugin"
     Author = "Ryan Young"
+    Version = "1.1.0"
 
     GameName = "Train Simulator"
     GameShortName = "railworks"
     GameBinary = "RailWorks.exe"
-    GameValidShortNames = ["ts1"]
     GameDataPath = ""
     GameSteamId = "24010"
-
-    def version(self):
-        # Don't forget to import mobase!
-        return mobase.VersionInfo(1, 0, 0, mobase.ReleaseType.FINAL)
+    GameSupportURL = (
+        r"https://github.com/ModOrganizer2/modorganizer-basic_games/wiki/"
+        "Game:-Train-Simulator-Classic"
+    )
 
     def executables(self):
+        game_directory = self.gameDirectory()
+        executables: list[tuple[str, str]] = [
+            ("32-bit", "RailWorks.exe"),
+            ("64-bit", "RailWorks64.exe"),
+            ("64-bit, DirectX 12", "RailWorksDX12_64.exe"),
+        ]
         return [
             mobase.ExecutableInfo(
-                "Train Simulator (32-bit)",
-                QFileInfo(self.gameDirectory(), "RailWorks.exe"),
-            ),
-            mobase.ExecutableInfo(
-                "Train Simulator (64-bit)",
-                QFileInfo(self.gameDirectory(), "RailWorks64.exe"),
-            ),
+                f"Train Simulator ({name})",
+                QFileInfo(game_directory.absoluteFilePath(path)),
+            )
+            for name, path in executables
         ]
