fa5d0c75308e41a6119a957ecba10566d0c9d5b0
Trevor Paley <10186337+TheUnlocked@users.noreply.github.com>
2024-01-29 16:56:29 -0800

    Ran black on FF7R

--- games/game_finalfantasy7remake.py
+++ games/game_finalfantasy7remake.py
@@ -4,6 +4,7 @@ from typing import List
 
 from ..basic_game import BasicGame
 
+
 class FinalFantasy7RemakeGame(BasicGame, mobase.IPluginFileMapper):
     Name = "Final Fantasy VII Remake Support Plugin"
     Author = "TheUnlocked"
@@ -14,7 +15,7 @@ class FinalFantasy7RemakeGame(BasicGame, mobase.IPluginFileMapper):
     GaneNexusHame = "finalfantasy7remake"
     GameSteamId = 1462040
     GameBinary = "ff7remake.exe"
-    GameSaveExtension = "sav"    
+    GameSaveExtension = "sav"
     # This is a placeholder. In order to properly apply load order to mods, custom mapping is used below.
     GameDataPath = "_ROOT"
 
@@ -24,40 +25,45 @@ class FinalFantasy7RemakeGame(BasicGame, mobase.IPluginFileMapper):
 
     def init(self, organizer: mobase.IOrganizer):
         return BasicGame.init(self, organizer)
-    
+
     def _modsPath(self):
         """The directory path of where .pak files from mods should go"""
-        return self.gameDirectory().absolutePath() + '/End/Content/Paks/~mods'
+        return self.gameDirectory().absolutePath() + "/End/Content/Paks/~mods"
 
     def mappings(self) -> List[mobase.Mapping]:
         mods = self._organizer.modList().allModsByProfilePriority()
         pak_priority_digits = math.floor(math.log10(len(mods))) + 1
 
-        def create_mod_mappings(mod: mobase.IModInterface, priority: int) -> List[mobase.Mapping]:
+        def create_mod_mappings(
+            mod: mobase.IModInterface, priority: int
+        ) -> List[mobase.Mapping]:
             # UE4 loads paks in alphabetical order, so we prefix the paks with their priority
-            pak_prefix = str(priority).zfill(pak_priority_digits) + '_'
+            pak_prefix = str(priority).zfill(pak_priority_digits) + "_"
 
             custom_mappings: List[mobase.Mapping] = []
 
-            def file_visitor(file_name: str, entry: mobase.FileTreeEntry) -> mobase.IFileTree.WalkReturn:
-                src_path = mod.absolutePath() + '/' + entry.path()
+            def file_visitor(
+                file_name: str, entry: mobase.FileTreeEntry
+            ) -> mobase.IFileTree.WalkReturn:
+                src_path = mod.absolutePath() + "/" + entry.path()
 
-                if entry.hasSuffix('pak'):
-                    dest_path = self._modsPath() + '/' + pak_prefix + entry.path()
+                if entry.hasSuffix("pak"):
+                    dest_path = self._modsPath() + "/" + pak_prefix + entry.path()
                 else:
-                    dest_path = self._modsPath() + '/' + entry.path()
+                    dest_path = self._modsPath() + "/" + entry.path()
 
-                custom_mappings.append(mobase.Mapping(
-                    src_path,
-                    dest_path,
-                    False,
-                ))
-                
+                custom_mappings.append(
+                    mobase.Mapping(
+                        src_path,
+                        dest_path,
+                        False,
+                    )
+                )
+
                 return mobase.IFileTree.WalkReturn.CONTINUE
 
             mod.fileTree().walk(file_visitor)
             return custom_mappings
-
 
         return [
             # Not applying load order modifications to overwrites is OK
@@ -71,5 +77,7 @@ class FinalFantasy7RemakeGame(BasicGame, mobase.IPluginFileMapper):
         ] + [
             mapping
             for i, mod_name in enumerate(mods)
-            for mapping in create_mod_mappings(self._organizer.modList().getMod(mod_name), i)
+            for mapping in create_mod_mappings(
+                self._organizer.modList().getMod(mod_name), i
+            )
         ]
