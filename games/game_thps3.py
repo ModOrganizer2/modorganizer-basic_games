db4dd5c39302135f52ef0192602f5c27a2c9f706
uwx <uwx@users.noreply.github.com>
2024-04-11 19:58:00 -0300

    Add THPS series support

---
+++ games/game_thps3.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+
+from PyQt6.QtCore import QFileInfo
+
+import mobase
+
+from ..basic_game import BasicGame
+
+
+class THPS3Game(BasicGame):
+
+    Name = "Tony Hawk's Pro Skater 3 Support Plugin"
+    Author = "Maxine"
+    Version = "1.0.0"
+
+    GameName = "Tony Hawk's Pro Skater 3"
+    GameShortName = "thps3"
+    GameBinary = "Skate3.exe"
+    GameDataPath = "Data"
+
+    def executables(self):
+        return [
+            mobase.ExecutableInfo(
+                "Tony Hawk's Pro Skater 3",
+                QFileInfo(self.gameDirectory().absoluteFilePath(self.binaryName())),
+            ),
+            mobase.ExecutableInfo(
+                "Tony Hawk's Pro Skater 3 Setup",
+                QFileInfo(self.gameDirectory().absoluteFilePath("THPS3Setup.exe")),
+            ),
+            mobase.ExecutableInfo(
+                "Tony Hawk's Pro Skater 3 (PARTYMOD)",
+                QFileInfo(self.gameDirectory().absoluteFilePath("THPS3.exe")),
+            ),
+            mobase.ExecutableInfo(
+                "PARTYMOD Configurator",
+                QFileInfo(self.gameDirectory().absoluteFilePath("partyconfig.exe")),
+            )
+        ]
