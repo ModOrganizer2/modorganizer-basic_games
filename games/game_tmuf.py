2bea13a5a6ae0c2d869e099cfc122d4edd7c0a71
uwx <uwx@users.noreply.github.com>
2024-04-11 19:23:26 -0300

    Support Trackmania United Forever

---
+++ games/game_tmuf.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+
+from __future__ import annotations
+
+from PyQt6.QtCore import QFileInfo
+
+import mobase
+
+from ..basic_game import BasicGame
+
+
+class TmufGame(BasicGame):
+
+    Name = "Trackmania United Forever Support Plugin"
+    Author = "uwx"
+    Version = "1.0.0"
+    Description = "Adds support for Trackmania United Forever game folder mods."
+
+    GameName = "Trackmania United Forever"
+    GameShortName = "tmuf"
+    GameNexusName = "trackmaniaunited"
+    GameNexusId = 1500
+    GameSteamId = 7200
+    GameBinary = "TmForeverLauncher.exe"
+    GameDataPath = ""
+
+    def executables(self):
+        return [
+            mobase.ExecutableInfo(
+                "Trackmania United Forever",
+                QFileInfo(
+                    self.gameDirectory(),
+                    self.binaryName(),
+                ),
+            ),
+        ]
