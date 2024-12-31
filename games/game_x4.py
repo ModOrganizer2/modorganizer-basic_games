c0b44c152efbf4023fd9b23bf17d455fc753d63d
Twinki <Twinki14@users.noreply.github.com>
2024-12-31 04:02:37 -0500

    Add X4: Foundations supporting plugin (#168)

---
+++ games/game_x4.py
@@ -0,0 +1,17 @@
+from ..basic_game import BasicGame
+
+
+class X4FoundationsGame(BasicGame):
+    Name = "X4 Foundations Support Plugin"
+    Author = "Twinki,BrandonM4"
+    Version = "0.1.0"
+
+    GameName = "X4: Foundations"
+    GameShortName = "x4foundations"
+
+    GameDocumentsDirectory = "%DOCUMENTS%/Egosoft/X4"
+    GameBinary = "x4.exe"
+    GameDataPath = "extensions"
+
+    GameNexusId = 2659
+    GameSteamId = 392160
