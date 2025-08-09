b63f5820bfd8200ffb255b4b177b9b6379c6390d
Savathun <djschlatt@gmail.com>
2025-08-09 17:05:45 -0600

    move bg3 data checker class to baldursgate3/bg3_data_checker.py

---
+++ games/baldursgate3/bg3_data_checker.py
@@ -0,0 +1,34 @@
+import mobase
+
+from basic_features import BasicModDataChecker, utils
+
+
+class BG3ModDataChecker(BasicModDataChecker):
+    def dataLooksValid(
+        self, filetree: mobase.IFileTree
+    ) -> mobase.ModDataChecker.CheckReturn:
+        status = mobase.ModDataChecker.INVALID
+        rp = self._regex_patterns
+        for entry in filetree:
+            name = entry.name().casefold()
+            if rp.unfold.match(name):
+                if utils.is_directory(entry):
+                    status = self.dataLooksValid(entry)
+                else:
+                    status = mobase.ModDataChecker.INVALID
+                    break
+            elif rp.valid.match(name):
+                if status is mobase.ModDataChecker.INVALID:
+                    status = mobase.ModDataChecker.VALID
+            elif isinstance(entry, mobase.IFileTree):
+                status = (
+                    mobase.ModDataChecker.VALID
+                    if all(rp.valid.match(e.pathFrom(filetree)) for e in entry)
+                    else mobase.ModDataChecker.INVALID
+                )
+            elif rp.delete.match(name) or rp.move_match(name) is not None:
+                status = mobase.ModDataChecker.FIXABLE
+            else:
+                status = mobase.ModDataChecker.INVALID
+                break
+        return status
--- games/game_baldursgate3.py
+++ games/game_baldursgate3.py
@@ -17,6 +17,7 @@ from typing import Any, Callable, Optional
 from xml.etree import ElementTree
 from xml.etree.ElementTree import Element
 
+import mobase
 import yaml
 from PyQt6 import QtCore
 from PyQt6.QtCore import (
@@ -37,47 +38,12 @@ from PyQt6.QtWidgets import (
     QProgressDialog,
 )
 
-import mobase
-
 from ..basic_features import (
     BasicGameSaveGameInfo,
     BasicLocalSavegames,
-    BasicModDataChecker,
     GlobPatterns,
-    utils,
 )
 from ..basic_game import BasicGame
-
-
-class BG3ModDataChecker(BasicModDataChecker):
-    def dataLooksValid(
-        self, filetree: mobase.IFileTree
-    ) -> mobase.ModDataChecker.CheckReturn:
-        status = mobase.ModDataChecker.INVALID
-        rp = self._regex_patterns
-        for entry in filetree:
-            name = entry.name().casefold()
-            if rp.unfold.match(name):
-                if utils.is_directory(entry):
-                    status = self.dataLooksValid(entry)
-                else:
-                    status = mobase.ModDataChecker.INVALID
-                    break
-            elif rp.valid.match(name):
-                if status is mobase.ModDataChecker.INVALID:
-                    status = mobase.ModDataChecker.VALID
-            elif isinstance(entry, mobase.IFileTree):
-                status = (
-                    mobase.ModDataChecker.VALID
-                    if all(rp.valid.match(e.pathFrom(filetree)) for e in entry)
-                    else mobase.ModDataChecker.INVALID
-                )
-            elif rp.delete.match(name) or rp.move_match(name) is not None:
-                status = mobase.ModDataChecker.FIXABLE
-            else:
-                status = mobase.ModDataChecker.INVALID
-                break
-        return status
 
 
 class BG3Game(BasicGame, mobase.IPluginFileMapper):
@@ -145,8 +111,9 @@ class BG3Game(BasicGame, mobase.IPluginFileMapper):
 
     def init(self, organizer: mobase.IOrganizer) -> bool:
         super().init(organizer)
+        from baldursgate3 import bg3_data_checker
         self._register_feature(
-            BG3ModDataChecker(
+            bg3_data_checker.BG3ModDataChecker(
                 GlobPatterns(
                     valid=[
                         "*.pak",
