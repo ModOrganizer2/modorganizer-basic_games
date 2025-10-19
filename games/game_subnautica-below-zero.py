755506c41bd7ae303111df9e707fdee593b39ca6
Zash <zash@fantasymail.de>
2025-10-19 13:49:30 +0200

    Subnautica BZ: remove inherited fields

--- games/game_subnautica-below-zero.py
+++ games/game_subnautica-below-zero.py
@@ -6,8 +6,6 @@ from . import game_subnautica  # namespace to not load SubnauticaGame here, too!
 
 class SubnauticaBelowZeroGame(game_subnautica.SubnauticaGame):
     Name = "Subnautica Below Zero Support Plugin"
-    Author = "dekart811, Zash"
-    Version = "2.2"
 
     GameName = "Subnautica: Below Zero"
     GameShortName = "subnauticabelowzero"
@@ -16,13 +14,10 @@ class SubnauticaBelowZeroGame(game_subnautica.SubnauticaGame):
     GameSteamId = 848450
     GameEpicId = "foxglove"
     GameBinary = "SubnauticaZero.exe"
-    GameDataPath = "_ROOT"
-    GameDocumentsDirectory = "%GAME_PATH%"
     GameSupportURL = (
         r"https://github.com/ModOrganizer2/modorganizer-basic_games/wiki/"
         "Game:-Subnautica:-Below-Zero"
     )
-    GameSavesDirectory = r"%GAME_PATH%\SNAppData\SavedGames"
 
     _game_extra_save_paths = [
         r"%USERPROFILE%\Appdata\LocalLow\Unknown Worlds"
