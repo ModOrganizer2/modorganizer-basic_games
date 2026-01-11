e4fbf38308a5df6dc236c7f6360f2ea77f80c5a7
Zash <zash@fantasymail.de>
2026-01-11 15:56:52 +0100

    Cyberpunk plugin version bump 3.0.1

--- games/game_cyberpunk2077.py
+++ games/game_cyberpunk2077.py
@@ -200,7 +200,7 @@ class ModListFileManager(dict[_MOD_TYPE, ModListFile]):
 class Cyberpunk2077Game(BasicGame):
     Name = "Cyberpunk 2077 Support Plugin"
     Author = "6788, Zash"
-    Version = "3.0.0"
+    Version = "3.0.1"
 
     GameName = "Cyberpunk 2077"
     GameShortName = "cyberpunk2077"
